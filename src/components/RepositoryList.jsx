1e6cec2afa486c70e703541acbcf81761525a521
Guilherme Henrique <guilherme.alvs@hotmail.com>
2021-03-27 19:23:30 -0300

    Chamada http

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,24 +1,24 @@
+import { useState, useEffect } from 'react';
+
 import { RepositoryItem } from './RepositoryItem';
 
-const repository = {
-  name: 'name',
-  description: 'description',
-  link: 'http://google.com'
-}
+import '../styles/repositories.scss';
 
 export function RepositoryList(){
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos').then(response => response.json()).then(data => setRepositories(data));
+  }, []);
+
   return(
     <section className="repository-list">
       <h1>Lista de Repósitorios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repo) => {
+        return <RepositoryItem key={repo.name} repository={repo} />
+        })}
       </ul>
     </section>
   );
