ccc84db50c25176ef0ac61f1ae4a41b91f8e27fe
Guilherme Henrique <guilherme.alvs@hotmail.com>
2021-03-27 21:13:04 -0300

    Propriedades no React

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,40 +1,24 @@
+import { RepositoryItem } from './RepositoryItem';
+
+const repository = {
+  name: 'name',
+  description: 'description',
+  link: 'http://google.com'
+}
+
 export function RepositoryList(){
   return(
     <section className="repository-list">
       <h1>Lista de Repósitorios</h1>
 
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in react</p>
-          <a href="">Acessar repositório</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in react</p>
-          <a href="">Acessar repositório</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in react</p>
-          <a href="">Acessar repositório</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in react</p>
-          <a href="">Acessar repositório</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in react</p>
-          <a href="">Acessar repositório</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in react</p>
-          <a href="">Acessar repositório</a>
-        </li>
-        
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   );
